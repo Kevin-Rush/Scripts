3d1cd963b4877730f8a407696d8e53cc0bce0d86
Kevin Rush <kevinrush.net@gmail.com>
2022-10-31 12:55:54 +0100

    data cleaned and organized

--- CleanSleepData.py
+++ CleanSleepData.py
@@ -7,6 +7,7 @@ data = data.iloc[:,:-21] #Get rid of the 21 unammed columns at the end of the cs
 data['naps'] = data['naps'].fillna(0)
 #print(data.head())
 
+data = data.loc[:,['date', 'naps', 'start', 'stop', 'shallowSleepTime', 'deepSleepTime', 'REMTime', 'wakeTime']]
 
 for x in data.index:
     if data.loc[x, 'naps'] != 0:
@@ -14,11 +15,12 @@ for x in data.index:
     if data.loc[x, 'start'] == data.loc[x, 'stop']: #sleep start and end date are the same, therefore a bad entry
         data.drop(x, inplace = True)#remove bad entry
 
-data.start = data.start.replace(r'\+0000', '', regex=True)
+data.start = data.start.replace(r'\+0000', '', regex=True) #clean up start and stop data
 data.stop = data.stop.replace(r'\+0000', '', regex=True)
 
 print(data)
 
-data['totalSleepTime'] = round((data['deepSleepTime'] + data["shallowSleepTime"] + data["REMTime"])/60, 2) #add up all sleep time, create new culumn and round to 2 decimals
+data['totalSleepTimeMin'] = data['deepSleepTime'] + data["shallowSleepTime"] + data["REMTime"] #add up all sleep time, create new culumn and round to 2 decimals
+data['totalSleeptimeHours'] = round(data['totalSleepTimeMin']/60, 2)
 
 print(data)
