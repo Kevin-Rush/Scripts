91a0fc19d62d3a6b76ce62a24ac76c1d331e8834
Kevin Rush <kevinrush.net@gmail.com>
2022-10-31 09:00:30 +0100

    start and stop editing done

--- CleanSleepData.py
+++ CleanSleepData.py
@@ -5,11 +5,13 @@ data = pd.read_csv(r'C:\Users\kevin\Documents\Personal\Athletics\SmartWatchData\
 data = data.iloc[:,:-21] #Get rid of the 21 unammed columns at the end of the csv file
 
 for x in data.index:
-    print(data.loc[x, "start"])
     if data.loc[x, "start"] == data.loc[x, "stop"]: #sleep start and end date are the same, therefore a bad entry
         data.drop(x, inplace = True)#remove bad entry
     
-    
+print(data.head())
+
+data.start = data.start.replace(r'\+0000', '', regex=True)
+data.stop = data.stop.replace(r'\+0000', '', regex=True)
 
 
 print(data.head())
