e81c5961d5cdf34b3fb37846b7c9c83739078774
Kevin Rush <kevinrush.net@gmail.com>
2022-11-08 04:22:31 +0100

    libraries split and cleaning

--- requirements_check.py
+++ requirements_check.py
@@ -19,5 +19,11 @@ for i in reqs_file.readlines():
 
 excluded_libs = []
 for i in libs_file.readlines():
-    lib_name = i.split("=", 1)[0]
-    libraries.append(lib_name)
+    if i[0] == "#":
+        continue
+    lib_name = i.split(" ")
+    
+    if lib_name[-1] == "\n":
+        lib_name.pop(-1)
+    lib_name[-1] = lib_name[-1].split("\n")[0]
+    print(lib_name)
