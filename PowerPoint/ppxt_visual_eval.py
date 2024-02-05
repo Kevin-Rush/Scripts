09752b53e1b11e37ec2a38178e1c68fbe1365912
Kevin Rush <kevinrush.net@gmail.com>
2024-02-05 17:41:13 +0100

    convert slides to pdf, convert pdf to images successful

--- PowerPoint/ppxt_visual_eval.py
+++ PowerPoint/ppxt_visual_eval.py
@@ -1,18 +1,29 @@
-from pptx import Presentation
-from PIL import Image
-import io
+import sys
+import os
+import glob
+import win32com.client
+from pdf2image import convert_from_path
+from wand.image import Image
 
-def save_slides_as_images(pptx_file):
-    presentation = Presentation(pptx_file)
-    
-    for i, slide in enumerate(presentation.slides):
-        for shape in slide.shapes:
-            if hasattr(shape, "image"):
-                image = shape.image
-                image_file = io.BytesIO(image.blob)
-                pil_image = Image.open(image_file)
-                pil_image.save(f"slide_{i+1}.png")
 
-# Usage
-ppxt_file = "C:/Users/kevin/Downloads/test_file.pptx"
-save_slides_as_images(ppxt_file)
+def convert(files, formatType = 32):
+    powerpoint = win32com.client.Dispatch("Powerpoint.Application")
+    powerpoint.Visible = 1
+    for filename in files:
+        newname = os.path.splitext(filename)[0] + ".pdf"
+        deck = powerpoint.Presentations.Open(filename)
+        deck.SaveAs(newname, formatType)
+        deck.Close()
+    powerpoint.Quit()
+
+def pdf_to_images(pdf_file):
+    # Convert PDF to images using wand
+    with Image(filename=pdf_file, resolution=300) as img:
+        img.compression_quality = 99
+        img.save(filename="output.jpg")
+
+files = glob.glob(r'C:\Users\kevin\Downloads\test_file.pptx')
+convert(files)
+
+pdf_file = "C:/Users/kevin/Downloads/test_file.pdf"
+pdf_to_images(pdf_file)
