7b026b81fc35a654b36a11e65dbc1b9c9d9487fa
Kevin Rush <kevinrush.net@gmail.com>
2024-02-08 18:17:34 +0100

    stable

--- PowerPoint/slide_deck_generator.py
+++ PowerPoint/slide_deck_generator.py
@@ -3,7 +3,7 @@ import json
 
 # Create a presentation object
 presentation = Presentation(r"C:\Users\kevin\Documents\Coding\Scripts\PowerPoint\base_presentation.pptx")
-json_file = r"C:\Users\kevin\Documents\Coding\Scripts\PowerPoint\generative_ai_presentation_script.json"
+json_file = r"C:\Users\kevin\Documents\Coding\Scripts\PowerPoint\example_presentation_script.json"
 
 #read in the script from the json file
 with open(json_file, 'r') as file:
@@ -13,7 +13,6 @@ content = content["slides"]
 
 for i in range(len(content)):
 
-    #if the title is "Agenda" we want to use the Agenda slide layout
     if content[i]['title'] == "DECK TITLE":
         slide = presentation.slides.add_slide(presentation.slide_layouts[0])
         title = slide.shapes.title
@@ -52,6 +51,9 @@ for i in range(len(content)):
         slide = presentation.slides.add_slide(presentation.slide_layouts[5])
         title = slide.shapes.title
         title.text = content[i]['title']
+
+        slide_subtitle = slide.shapes[1]
+        slide_subtitle.text = content[i]['subtitle']
 
         slide_content = slide.shapes[2]
         slide_content.text = content[i]['content']
