1dfc61977f5dd88666efd09d78d99577d7c8f60b
Kevin Rush <kevinrush.net@gmail.com>
2024-02-22 13:39:16 +0100

    code refactoring

--- PowerPoint/course_evaluation/grammar_eval.py
+++ PowerPoint/course_evaluation/grammar_eval.py
@@ -7,11 +7,9 @@ import pandas as pd
 def grammar_evaluation(ppxt_filepath, sapling_api_key):
 
     presentation = Presentation(ppxt_filepath)
+    client = SaplingClient(api_key=sapling_api_key)
 
     for i, slide in enumerate(presentation.slides, start=1):
- 
-        client = SaplingClient
-
         for shape in slide.shapes:
 
             #access the text in all types of shapes
@@ -20,8 +18,6 @@ def grammar_evaluation(ppxt_filepath, sapling_api_key):
                     paragraph_text = "".join(run.text for run in paragraph.runs)
                     #send the slide text to sapling
                     edits = client.edits(paragraph_text, session_id='test_session')
-
-                    text = str(text)
                     edits = sorted(edits, key=lambda e: (e['sentence_start'] + e['start']), reverse=True)
                     for edit in edits:
                         start = edit['sentence_start'] + edit['start']
@@ -31,3 +27,12 @@ def grammar_evaluation(ppxt_filepath, sapling_api_key):
                             continue
                         text = text[: start] + edit['replacement'] + text[end:]
         return text
+    
+
+#open the file sapling_api_key.txt and read the api key
+with open("C:/Users/kevin/Documents/Coding/Scripts/sapling_api_key.txt", "r") as file:
+    sapling_api_key = file.read()
+
+response = grammar_evaluation("C:/Users/kevin/Downloads/course/Week 9 - GAI.pptx", sapling_api_key)
+
+print(response)
