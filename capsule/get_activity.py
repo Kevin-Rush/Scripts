fb724ea5027d10545c444ab302dad3052d403c8d
Kevin Rush <kevinrush.net@gmail.com>
2024-03-18 12:42:07 +0100

    created a file for collecting the notes logged on capsule

--- capsule/get_activity.py
+++ capsule/get_activity.py
@@ -10,7 +10,7 @@ headers = {
     'Authorization': f'Bearer {api_token}',
     'Accept': 'application/json'
 }
-url = 'https://api.capsulecrm.com/api/v2/parties'
+url = 'https://api.capsulecrm.com/api/v2/activitytypes'
 
 response = requests.get(url, headers=headers)
 
@@ -18,15 +18,12 @@ if response.status_code == 200:
     print('Successful Request')
     data = response.json()
     #save the extracted json data in a df
-    df = pd.DataFrame(data['parties'])
+    df = pd.DataFrame(data['activityTypes'])
     
     #print the comolumns in the df
     print(df.columns)
 
     print(df.head(1))
 
-    #save first name, last name, and email to a csv file
-    df.to_csv('participants.csv', columns=['firstName', 'lastName', 'emailAddresses'], index=False)
-    print('Emails saved to participants.csv')
 else:
     print(f'Request failed with status code {response.status_code}')
