512092ff1eba5173624caaa5a2e382618e9ec91e
Kevin Rush <kevinrush.net@gmail.com>
2024-03-18 15:51:17 +0100

    properly accessing actiity in get activity, and basic updates to participants

--- capsule/get_activity.py
+++ capsule/get_activity.py
@@ -10,20 +10,46 @@ headers = {
     'Authorization': f'Bearer {api_token}',
     'Accept': 'application/json'
 }
-url = 'https://api.capsulecrm.com/api/v2/activitytypes'
+parties_url = 'https://api.capsulecrm.com/api/v2/parties'
 
-response = requests.get(url, headers=headers)
+activity_url = 'https://api.capsulecrm.com/api/v2/activitytypes'
 
-if response.status_code == 200:
-    print('Successful Request')
-    data = response.json()
-    #save the extracted json data in a df
-    df = pd.DataFrame(data['activityTypes'])
-    
-    #print the comolumns in the df
-    print(df.columns)
-
-    print(df.head(1))
-
-else:
+response = requests.get(parties_url, headers=headers)
+if response.status_code != 200:
     print(f'Request failed with status code {response.status_code}')
+    exit()
+print('Successful Request: ', response.status_code)
+
+data = response.json()
+
+#save the extracted json data in a df
+df = pd.DataFrame(data['parties'])
+
+#print the comolumns in the df
+print("columns: ", df.columns)
+print(df.head())
+
+#for i in range(len(df["id"])):
+#print("df id: ", df["id"][i])
+
+entity = "parties"
+entityId = 230533890
+print("entityId: ", entityId)
+
+entries_url = f"https://api.capsulecrm.com/api/v2/{entity}/{entityId}/entries"
+
+response = requests.get(entries_url, headers=headers)
+if response.status_code != 200:
+    print(f'Request failed with status code {response.status_code}')
+    exit()
+print('Successful Request: ', response.status_code)
+
+data = response.json()
+print("data: ", data)
+#save the extracted json data in a df
+temp_df = pd.DataFrame(data["entries"])
+
+#print the comolumns in the df
+print("columns: ", temp_df.columns)
+
+print(temp_df.head(1))
--- capsule/participants.py
+++ capsule/participants.py
@@ -14,19 +14,20 @@ url = 'https://api.capsulecrm.com/api/v2/parties'
 
 response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
-    print('Successful Request')
-    data = response.json()
-    #save the extracted json data in a df
-    df = pd.DataFrame(data['parties'])
-    
-    #print the comolumns in the df
-    print(df.columns)
-
-    print(df.head(1))
-
-    #save first name, last name, and email to a csv file
-    df.to_csv('participants.csv', columns=['firstName', 'lastName', 'emailAddresses'], index=False)
-    print('Emails saved to participants.csv')
-else:
+if response.status_code != 200:
     print(f'Request failed with status code {response.status_code}')
+
+print('Successful Request')
+data = response.json()
+#save the extracted json data in a df
+df = pd.DataFrame(data['parties'])
+
+#print the comolumns in the df
+print(df.columns)
+
+print(df.head(1))
+
+#save first name, last name, and email to a csv file
+df.to_csv('participants.csv', columns=['firstName', 'lastName', 'emailAddresses'], index=False)
+print('Emails saved to participants.csv')
+
