637e13279cab4b738d43ab3aaeba42f7e0a407a6
Kevin Rush <kevinrush.net@gmail.com>
2024-03-21 11:14:03 +0100

    updated to be more useable

--- PowerPoint/course_evaluation/utils.py
+++ PowerPoint/course_evaluation/utils.py
@@ -1,7 +1,7 @@
 #useful functions used across multiple scripts
 
-def print_loader_df (df, i):
+def print_loader_df (length, i):
     #print a loading bar for a dataframe
-    percentage = int((i / len(df)) * 100)
+    percentage = int((i / length) * 100)
     loading_bar = '#' * (percentage // 2 + 2) + '-' * (50 - percentage // 2)
     print(f"\r[{loading_bar}] {percentage}%", end='')
