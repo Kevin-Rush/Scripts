bcd987e1bd9c8bdf557edce711a2b5eeaeed49de
Kevin Rush <kevinrush.net@gmail.com>
2024-03-21 06:40:14 +0100

    code refactoring for modularity

--- PowerPoint/course_evaluation/ppxt_vision.py
+++ PowerPoint/course_evaluation/ppxt_vision.py
@@ -12,11 +12,20 @@ def encode_image(image_path):
 
 def run_slide_eval(image_path, api_key):
     # This function takes a folder of images and an API key and returns a list of responses from the GPT-4 Vision model
-    openai.api_key = api_key
-    
-    print(f"{Fore.GREEN}\n-----------------Image Processed-----------------{Fore.RESET}")
     print(image_path)
-    # Full path to the image file
+    prompt = "You are a powerpoint slide evaluator. Your job is to review the slide and provide feedback on the clarity, simplicity, and visual appeal of the slide. For clarity and simplicity I want you to tell me if the information being presented is given in a direct and pithy way that does not use any extravegant phrasing (extravegant phrasing includes but is not restricted to: 'delve', 'utilize', 'the art and science of', 'enter the world of', etc.). For visual appeal, I want you to tell me if the information on the slide is presented in an effective way. Ensure that you say 1 good thing about the slide, and then provide 3 specific and actionable pieces feedback. Keep responses short and direct yet professional and only consider the text I've given, do not think beyond the slide."
+
+    response = call_gpt_vision(image_path, prompt, api_key)
+    print(f"{Fore.GREEN}\n-----------------Image Processed-----------------{Fore.RESET}")
+
+    return response
+
+def read_SM_slide(slide):
+    return 
+
+def call_gpt_vision(image_path, prompt, api_key):
+    # This function takes a folder of images and an API key and returns a list of responses from the GPT-4 Vision model
+    openai.api_key = api_key
     base64_image = encode_image(image_path)
 
     headers = {
@@ -32,7 +41,7 @@ def run_slide_eval(image_path, api_key):
         "content": [
             {
             "type": "text",
-            "text": "You are a powerpoint slide evaluator. Your job is to review the slide and provide feedback on the clarity, simplicity, and visual appeal of the slide. For clarity and simplicity I want you to tell me if the information being presented is given in a direct and pithy way that does not use any extravegant phrasing (extravegant phrasing includes but is not restricted to: 'delve', 'utilize', 'the art and science of', 'enter the world of', etc.). For visual appeal, I want you to tell me if the information on the slide is presented in an effective way. Ensure that you say 1 good thing about the slide, and then provide 3 specific and actionable pieces feedback. Keep responses short and direct yet professional and only consider the text I've given, do not think beyond the slide."
+            "text": prompt
             },
             {
             "type": "image_url",
@@ -50,6 +59,3 @@ def run_slide_eval(image_path, api_key):
     response = response.json()
 
     return response
-
-def read_SM_slide(slide):
-    return 
