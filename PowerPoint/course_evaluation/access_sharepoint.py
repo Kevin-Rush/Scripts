ed31b1f55126598e3e5e5c2c9e80b25f69e851c5
Kevin Rush <kevinrush.net@gmail.com>
2024-03-21 06:33:43 +0100

    accessing sharepoint, unstable

--- PowerPoint/course_evaluation/access_sharepoint.py
+++ PowerPoint/course_evaluation/access_sharepoint.py
@@ -1,26 +1,26 @@
 import os
 import sharepy
-
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.user_credential import UserCredential
+
 
 #read in the username, password, and sharepoint link from a file
 with open("sharepoint_credentials.txt", "r") as file:
     username = file.readline().strip()
     password = file.readline().strip()
-    site_url = file.readline().strip()
+    sharepoint_url = file.readline().strip()
     folder_url = file.readline().strip()
 
-ctx_auth = AuthenticationContext(site_url)
-print('Authenticating...')
-if ctx_auth.acquire_token_for_user(username, password):
-    print('Authenticated')
-    ctx = ClientContext(site_url, ctx_auth)
-    folder = ctx.web.get_folder_by_server_relative_url(folder_url)
-    print('Getting folder...')
-    ctx.load(folder)
-    print('Loading folder...')
+
+user_credential = UserCredential(username, password)
+ctx = ClientContext(sharepoint_url).with_credentials(user_credential)
+
+folder = ctx.web.get_folder_by_server_relative_url(folder_url)
+ctx.load(folder)
+try:
     ctx.execute_query()
-    print('Folder URL: ', folder.properties['ServerRelativeUrl'])
-else:
-    print(ctx_auth.get_last_error())
+    print("Access to the folder is successful!")
+    print("Folder properties: ", folder.properties)
+except Exception as e:
+    print("Failed to access the folder. Error: ", e)
