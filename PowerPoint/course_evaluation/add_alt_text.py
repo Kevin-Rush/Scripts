2f1b12f462f4b482d1499b9cdad756f2471327bb
Kevin Rush <kevinrush.net@gmail.com>
2024-03-22 14:37:15 +0100

    add alt text to am image IF it's not marked as decorative

--- PowerPoint/course_evaluation/add_alt_text.py
+++ PowerPoint/course_evaluation/add_alt_text.py
@@ -1,15 +1,23 @@
 from pptx import Presentation
+from pptx.oxml.ns import nsdecls, _nsmap
 
 def add_alt_text_to_image(ppxt_file, slide_index, alt_text):
+
+    # Add the adec namespace
+    _nsmap["adec"] = "http://schemas.microsoft.com/office/drawing/2017/decorative"
+
     # Get the specified slide
     slide = presentation.slides[slide_index]
 
     # Iterate through all shapes in the slide
     for shape in slide.shapes:
         if shape.shape_type == 13:  # Check if the shape is an image
-            print(shape._element._nvXxPr.cNvPr.attrib.get("descr", ""))
-            shape._element._nvXxPr.cNvPr.attrib["descr"] = alt_text  # Set the alt text for the image
-            print(shape._element._nvXxPr.cNvPr.attrib.get("descr", ""))
+            #check if an image is marked as decorative
+            adec_decoratives = shape._element._nvXxPr.cNvPr.xpath(".//adec:decorative[@val='1']")
+            if not adec_decoratives:
+                if shape._element._nvXxPr.cNvPr.attrib.get("descr", "") == "":
+                    print("START:"+shape._element._nvXxPr.cNvPr.attrib.get("descr", "")+":FINISH")
+                    shape._element._nvXxPr.cNvPr.attrib["descr"] = alt_text  # Set the alt text for the image
 
     # Save the modified presentation
     presentation.save(ppxt_file)
@@ -17,7 +25,7 @@ def add_alt_text_to_image(ppxt_file, slide_index, alt_text):
 # Usage example
 ppxt_file = "C:/Users/kevin/Downloads/Week 06.pptx"
 presentation = Presentation(ppxt_file)
-alt_text = 'Alt text for the image'
+alt_text = '2nd Alt Text Run'
 
 for i in range(len(presentation.slides)):
     slide_i = i
