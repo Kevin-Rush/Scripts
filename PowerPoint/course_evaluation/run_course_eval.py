edfa4f6a8472377b696e65c8401f2aa11051560e
Kevin Rush <kevinrush.net@gmail.com>
2024-03-22 14:39:56 +0100

    file renaming

--- PowerPoint/course_evaluation/run_course_eval.py
+++ PowerPoint/course_evaluation/run_course_eval.py
@@ -1,7 +1,6 @@
 import ppxt_processor
 import convert_slides_to_pdf_to_image
-import ppxt_visual_eval
-import ppxt_written_eval
+import ppxt_text as ppxt_text
 import grammar_eval
 from extract_script import extract_script
 import glob
@@ -9,46 +8,43 @@ from colorama import Fore, Style
 import pandas as pd
 import os
 
-with open("C:/Users/kevin/Documents/Coding/Scripts/gpt_api_key.txt", "r") as file:
-    gpt_api_key = file.read()
-
 with open("C:/Users/kevin/Documents/Coding/Scripts/sapling_api_key.txt", "r") as file:
     sapling_api_key = file.read()
 
 with open("C:/Users/kevin/Documents/Coding/Scripts/PowerPoint/course_evaluation/common_generated_terms.txt", "r") as file:
     common_generated_terms = file.read().splitlines()
-    print(common_generated_terms)
 
 
-ppxt_file_glob = glob.glob(r'C:\Users\kevin\Downloads\Week 11 - GAI.pptx')
+ppxt_file_glob = glob.glob(r'C:\Users\kevin\Downloads\Week 06.pptx')
 
 output_file = "extracted_script.txt"
 root = "C:/Users/kevin/Documents/Coding/Scripts/PowerPoint/course_evaluation/"
 
 print(f"{Fore.RESET}Start")
+
+print(f"{Fore.YELLOW}---------------------Create Slide Images---------------------{Fore.RESET}")
+convert_slides_to_pdf_to_image.run(ppxt_file_glob, root)
+print(f"{Fore.GREEN}---------------------Image Creation Successful---------------------{Fore.RESET}")
+
 #grammar_eval.evaluate(ppxt_file_glob[0], sapling_api_key)
 #print(f"{Fore.GREEN}---------------------Grammar Eval---------------------{Fore.RESET}")
 df = ppxt_processor.process(ppxt_file_glob[0])
 print(f"{Fore.GREEN}---------------------Slide Deck Contents Saved in DataFrame---------------------{Fore.RESET}")
 
-df = ppxt_written_eval.count_overused_words(df, common_generated_terms)
+df = ppxt_text.count_overused_words(df, common_generated_terms)
 print(df)
 #sum entire columns in the df   
 for word in common_generated_terms:
     print(f"{word}: {df[word].sum()}")
+print(f"{Fore.GREEN}---------------------Common Generated Terms Counted---------------------{Fore.RESET}")
 
 df.to_csv(root + "ppxt_pre_eval.csv", index=False)
-print(f"{Fore.GREEN}---------------------Common Generated Terms Counted---------------------{Fore.RESET}")
 print(f"{Fore.GREEN}---------------------Output Saved---------------------{Fore.RESET}")
 
-#df = pd.read_csv(root + "ppxt_pre_eval.csv")
-
-print(f"{Fore.YELLOW}---------------------Converter Deck to Images---------------------{Fore.RESET}")
-convert_slides_to_pdf_to_image.run(ppxt_file_glob, root)
-print(f"{Fore.GREEN}---------------------Conversion Successful---------------------{Fore.RESET}")
+# df = pd.read_csv(root + "ppxt_pre_eval.csv")
 
 print(f"{Fore.YELLOW}---------------------Evaluate the Deck---------------------{Fore.RESET}")
-df = ppxt_written_eval.evaluate(df, gpt_api_key)
+df = ppxt_text.evaluate(df)
 print(f"{Fore.GREEN}---------------------Evaluation Successful---------------------{Fore.RESET}")
 
 #run through all the images in the folder ppxt_images and delete them
