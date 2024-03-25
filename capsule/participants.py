29004c7f613ef6c0940031f1cd9104a9fb80da9c
Kevin Rush <kevinrush.net@gmail.com>
2024-03-25 14:56:40 +0100

    updated participant collection method

--- capsule/participants.py
+++ capsule/participants.py
@@ -10,24 +10,63 @@ headers = {
     'Authorization': f'Bearer {api_token}',
     'Accept': 'application/json'
 }
-url = 'https://api.capsulecrm.com/api/v2/parties'
 
-response = requests.get(url, headers=headers)
+# success = True
+# i = 1
+# df = pd.DataFrame()
+# while success == True:
 
-if response.status_code != 200:
-    print(f'Request failed with status code {response.status_code}')
+# url = f'https://api.capsulecrm.com/api/v2/parties'
+# print(url)
+# i += 1
 
-print('Successful Request')
-data = response.json()
-#save the extracted json data in a df
-df = pd.DataFrame(data['parties'])
+# response = requests.get(url, headers=headers)
+
+# if response.status_code != 200:
+#     print(f'Request failed with status code {response.status_code}')
+#     # success = False
+
+# print('Successful Request')
+# data = response.json()
+# #print(data)
+
+
+# #add the extracted json to the df
+# df = pd.DataFrame(data['parties'])
+# # print(len(df))
 
 #print the comolumns in the df
-print(df.columns)
+#print(df.columns)
 
-print(df.head(1))
+# print(df.head(-1))
 
+#read the party_ids.txt file and create a list of party ids
+with open("party_ids.txt", "r") as file:
+    party_ids = file.read().splitlines()
+
+df = pd.DataFrame()
+for id in party_ids:
+    url = f'https://api.capsulecrm.com/api/v2/parties/{id}'
+    print(url)
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f'Request failed with status code {response.status_code}')
+    data = response.json()
+    print(data)
+    if data['party']['type'] != 'organization':
+        df_temp = pd.DataFrame([data['party']])
+        df = df.append(df_temp, ignore_index=True)
+    # print(df.columns)
+    # print(df.head(1))
 #save first name, last name, and email to a csv file
-df.to_csv('participants.csv', columns=['firstName', 'lastName', 'emailAddresses'], index=False)
+
+df = df[['firstName', 'lastName', 'emailAddresses']]
+print(df.head())
+for i in range(len(df['emailAddresses'])):
+    if df['emailAddresses'][i] != []:
+        df['emailAddresses'][i] = df['emailAddresses'][i][0]["address"]
+print(df.head())
+
+df.to_csv('participants.csv', index=False)
 print('Emails saved to participants.csv')
 
